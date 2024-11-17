0a23e908f9a8e89fbfb6b4aa251dfc74e31c65da
leomick <83542885+leomick@users.noreply.github.com>
2024-11-17 12:32:38 -0500

    MAKE IT ALL AN INTERFACE INTERFACES RULE THE WORLD!

--- plugin.go
+++ plugin.go
@@ -1,22 +1,13 @@
 package plugin
 
-// Plugin is the plugin itself. It must implement the Functions interface
-type Plugin struct {
-	// Channel that sends the download progress, number between 0 and 1, 0 is 0% and 1 is 100%
-	Progress chan float64
-	// The Data variable stores any information you might need to save, it is only accessed by you. A struct would be a good option for storing multiple variables
-	Data any
-}
-
-// Functions is the functions that a Plugin must have to be valid
-type Functions interface {
+// Plugin is the plugin's functions
+type Plugin interface {
 	// Download starts the download
 	Download(version string) error
 	// Install executes after the download is finished
 	Install(installDir string) error
 	// GetLatestVersion returns the version number of the latest version
 	GetLatestVersion() (string, error)
+	// Progress returns a channel that you sent a float of the progress between 0 and 1, it's kind of like an initialization function
+	Progress() <-chan float64
 }
-
-// New initializes the plugin
-type New func() (Plugin, error)
