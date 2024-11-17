637d1070af34a561e063d365a187e5842fe260eb
leomick <83542885+leomick@users.noreply.github.com>
2024-11-17 12:19:04 -0500

    Make the functions be part of an interface

--- plugin.go
+++ plugin.go
@@ -1,15 +1,21 @@
 package plugin
 
+// Plugin is the plugin itself. It must implement the Functions interface
 type Plugin struct {
-	Download func(version string) error
-	// Executes when Download is finished, installDir is the directory to install in, it should only contain the binaries of the tool
-	Install func(installDir string) error
 	// Channel that sends the download progress, number between 0 and 1, 0 is 0% and 1 is 100%
 	Progress chan float64
-	// Returns the version number of the latest version available and an error if there is any
-	GetLatestVersion func() (string, error)
 	// The Data variable stores any information you might need to save, it is only accessed by you. A struct would be a good option for storing multiple variables
 	Data any
+}
+
+// Functions is the functions that a Plugin must have to be valid
+type Functions interface {
+	// Download starts the download
+	Download(version string) error
+	// Install executes after the download is finished
+	Install(installDir string) error
+	// GetLatestVersion returns the version number of the latest version
+	GetLatestVersion() (string, error)
 }
 
 // New initializes the plugin
