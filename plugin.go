8b06b43d82770d82cee8f61c2ddbe9cf37ede9a4
leomick <83542885+leomick@users.noreply.github.com>
2024-11-19 16:56:15 -0500

    Remove progress function and just pass a progress channel into the download function

--- plugin.go
+++ plugin.go
@@ -2,12 +2,10 @@ package mtvmplugin
 
 // Plugin is the plugin's functions
 type Plugin interface {
-	// Download starts the download
-	Download(version string) error
+	// Download starts the download, please send the download progress through the progress channel, send 0 for 0% and 1 for 100%
+	Download(version string, progress chan float64) error
 	// Install executes after the download is finished
 	Install(installDir string) error
 	// GetLatestVersion returns the version number of the latest version
 	GetLatestVersion() (string, error)
-	// Progress returns a channel that you sent a float of the progress between 0 and 1, it's kind of like an initialization function
-	Progress() *chan float64
 }
