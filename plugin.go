59fe4d81c51a7d0e493e48b7a5f79d27ab9ffb24
leomick <83542885+leomick@users.noreply.github.com>
2024-11-25 13:52:14 -0500

    Add GetCurrentVersion function

--- plugin.go
+++ plugin.go
@@ -13,4 +13,7 @@ type Plugin interface {
 	// Use sets the current version. A path directory is provided for you to symlink or copy the binaries into (it is called path directory because it should be part of the system's path variable)
 	// An installDir is also provided which is where the version is installed.
 	Use(installDir string, pathDir string) error
+	// GetCurrentVersion gets the current set version and returns an empty string if a version is not set. An installDir is provided which is where all the versions of the plugins are installed.
+	// For example go 1.23.3 would be installed in installDir/1.23.3 A pathDir is also provided so you can evaluate the symlinks of the binaries in there to find the current version
+	GetCurrentVersion(installDir string, pathDir string) (string, error)
 }
